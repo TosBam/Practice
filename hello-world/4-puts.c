73feadae433688dff1f05ddd4073be5e2745efe7
TosBam <samuelson.os19@gmail.com>
2022-11-06 03:32:02 -0800

    this task done

---
+++ hello-world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to write a program using puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
+}
